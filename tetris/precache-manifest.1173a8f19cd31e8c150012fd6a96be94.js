37d0ad8906fb1144fae1c385627268e73021fdbf
Matt Lahut <mlahut@emmes.com>
2020-01-22 17:40:38 -0500

    Initial commit of tetris prod

---
+++ tetris/precache-manifest.1173a8f19cd31e8c150012fd6a96be94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0170698694fa2b29b47d596263b387",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "1464746bb2c735985c0a",
+    "url": "/tetris/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "82338f4e973c57fa7d1d",
+    "url": "/tetris/static/js/2.b610283b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tetris/static/js/2.b610283b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1464746bb2c735985c0a",
+    "url": "/tetris/static/js/main.a4e33f09.chunk.js"
+  },
+  {
+    "revision": "ef89fd4ae7f807e31101",
+    "url": "/tetris/static/js/runtime-main.e1b2be52.js"
+  }
+]);
